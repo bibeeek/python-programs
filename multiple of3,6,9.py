ce1c45ac9592a35c778820111848136aba171094
Bibek Pokhrel <119734061+bibeeek@users.noreply.github.com>
2022-12-12 16:06:08 +0545

    Add files via upload

---
+++ multiple of3,6,9.py
@@ -0,0 +1,52 @@
+"""multiple of 3 from(1-100)"""
+
+print("multiple of 3")
+y=[]
+for x in range(1,100):
+    mult=x*3
+    if mult>=100:
+       break
+    
+    y.append(mult)
+    
+    
+print(y)
+print("\n")
+
+  
+"""multiple of 6 from(1-100)"""
+
+print("multiple of 6")
+y=[]
+for x in range(1,100):
+    mult=x*6
+    if mult>=100:
+       break
+    
+    y.append(mult)
+    
+print(y)
+print("\n")
+
+
+"""multiple of 9 from(1-100)"""
+print("multiple of 9")
+y=[]
+for x in range(1,100):
+    mult=x*9
+    if mult>=100:
+       break
+    
+    y.append(mult)
+    
+print(y)
+  
+
+  
+    
+
+    
+  
+    
+
+
